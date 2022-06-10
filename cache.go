05a03c9178a5d048f912cdb7800d9af98e8353e7
Alex Scherba <alexscherba16@gmail.com>
2022-06-10 09:05:27 +0300

    cache solution

--- cache.go
+++ cache.go
@@ -2,22 +2,63 @@ package cache
 
 import "time"
 
+type cacheValue struct {
+	value    string
+	deadline time.Time
+}
+
+func (v *cacheValue) isExpired() bool {
+	if time.Until(v.deadline).Milliseconds() >= 0 || v.deadline.IsZero() {
+		return false
+	}
+	return true
+}
+
 type Cache struct {
+	Storage map[string]cacheValue
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		Storage: make(map[string]cacheValue),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c Cache) Get(key string) (string, bool) {
+	item, ok := c.Storage[key]
+	if ok == false {
+		return "", ok
+	}
 
+	if item.isExpired() {
+		delete(c.Storage, key)
+		return "", false
+	}
+
+	return item.value, true
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.Storage[key] = cacheValue{
+		value: value,
+	}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.Storage[key] = cacheValue{
+		value:    value,
+		deadline: deadline,
+	}
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) Keys() []string {
+	var keys []string
+
+	for key, item := range c.Storage {
+		if item.isExpired() {
+			continue
+		}
+		keys = append(keys, key)
+	}
+	return keys
 }
